d55711107ac2b9d3c1512a196e7a1c285d204f7d
kovalchuk14 <kovalcukanton682@gmail.com>
2025-09-24 15:18:31 +0200

    bag fix

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,7 +10,7 @@ router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
 
-router.patch('contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
 
 export default router;
