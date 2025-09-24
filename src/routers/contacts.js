3c838e8e5e1f6ec18b2f96648cf7b119acdfe95d
kovalchuk14 <kovalcukanton682@gmail.com>
2025-09-24 18:37:26 +0200

    new rout

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/CtrlWrapper.js";
-import { getContactsController, getContactByIdController, createContactController, patchContactController } from "../controllers/contacts.js";
+import { getContactsController, getContactByIdController, createContactController, patchContactController, deleteContactController } from "../controllers/contacts.js";
 const router = Router();
 
 
@@ -12,5 +12,6 @@ router.post('/contacts', ctrlWrapper(createContactController));
 
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
