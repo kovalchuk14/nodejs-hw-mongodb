27911f4a9756fee11e5519bac378c10052f6cfec
kovalchuk14 <kovalcukanton682@gmail.com>
2025-09-29 17:14:57 +0200

    email value update

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
-    email: Joi.string(),
+    email: Joi.string().email(),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home','personal').required(),
 });
@@ -12,7 +12,7 @@ export const createContactSchema = Joi.object({
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
     phoneNumber: Joi.string().min(3).max(20),
-    email: Joi.string(),
+    email: Joi.string().email(),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home','personal'),
 });
